08e80f82211e73dfabb917c4e547caad4265a9a1
Nick <nickmufson1@gmail.com>
2024-12-02 16:48:47 -0500

    update interests section

--- src/components/Interests/Interests.tsx
+++ src/components/Interests/Interests.tsx
@@ -15,15 +15,15 @@ const Interests = () => {
       <div className={styles.interestGroup}>
         <h3>Language Learning</h3>
         <p>
-          At the beginning of the year I started learning Spanish. I found an
-          interesting learning method called Comprehensible Input, which
-          involves simply watching, listening to, or reading content in your
-          target language and naturally acquiring it over time. The idea really
-          interested me because it didn't involve the things I disliked about
-          learning Spanish in highschool, like studying grammar or memorizing
-          flashcards. I've made a lot of progress and now enjoy listening to
-          Spanish podcasts while walking / running errands, typically about
-          philosophy, technology, or personal development.
+          At the beginning of the year I started learning Spanish. I found out
+          about a learning method called Comprehensible Input, which involves
+          watching, listening to, or reading content in your target language to
+          naturally acquire it over time. The idea really interested me because
+          it didn't involve the things I disliked about learning Spanish in
+          highschool, like studying grammar or trying to memorize vocabulary.
+          I've made a lot of progress and now enjoy listening to Spanish
+          podcasts while walking / running errands, typically about philosophy,
+          technology, or personal development.
         </p>
       </div>
       <div className={styles.interestGroup}>
